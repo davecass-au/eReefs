91d9c30540a9eba5cbaa64af920b45bb84eaa230
David Cass <david.charles.cass@gmail.com>
2023-09-25 06:57:49 +1000

    Code tidy

--- app/src/UI/ReefMap2.jsx
+++ app/src/UI/ReefMap2.jsx
@@ -1,9 +1,6 @@
 import '../styles/ReefMap.css'
-import { MapContainer, TileLayer, WMSTileLayer } from 'react-leaflet';
 import L from 'leaflet'
-import { Component, useEffect } from 'react';
 import React from 'react';
-
 
 
 export default class ReefMap2 extends React.Component {
@@ -21,7 +18,6 @@ export default class ReefMap2 extends React.Component {
             timeDimensionControl: true,
             scrollWheelZoom: true
         })
-        //m.setView([-15, -220], 6);
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,            
@@ -33,53 +29,16 @@ export default class ReefMap2 extends React.Component {
             timeDimension: true,
             timeDimensionControl: true,
             opacity: 0.5
-        }).addTo(m);
-        //var a = L.map('map')
-
-        //if (a != 'undefined' || a != null) {
-        //    a.remove();
-        //} 
-        //a.setView([37.7749, -122.4194], 13);
-        
-        //var b = L.map('map')
-        //b.setView([37.7749, -122.4194], 13);
-        //var m = L.map('map').remove();
-
+        }).addTo(m);        
     }
-//        //testmap = L.map('map').off();
-//        //var myMap = L.map('map').remove();
-//        /*var a = L.map('map').remove();*/
-//        //console.log(myMap)
-
-//        L.map('map').setView([37.7749, -122.4194], 13);
-//    }
-
-   
-    //useEffect(() => {
-
-    //    let map = L.map('map', {
-    //        center: [-15, -220],
-    //        zoom: 6,
-    //        scrollWheelZoom: true
-    //    })
-    //});
-
-
-    //L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //    maxZoom: 19,
-    //    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    //}).addTo(map);
-
-
+  
     render() {
         return (
-
             <>
                 <div id="map" className="mapContainer">
 
                 </div>
             </>
         )
-    }
-    
+    }    
 }
